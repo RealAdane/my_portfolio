b888424a1214a28cae1e11f7899187caca8f26e7
Adane <real.adane.adgo@gmail.com>
2023-12-13 19:56:14 +0200

    the actual page

---
+++ .dart_tool/flutter_build/10e1843101016572ee64d417a4098fc8/web_resources.d
@@ -0,0 +1,1 @@
+ build/web/favicon.ico build/web/index.html build/web/.DS_Store:  /Users/adaneadgo/repos/my_portfolio/web/favicon.ico /Users/adaneadgo/repos/my_portfolio/web/index.html /Users/adaneadgo/repos/my_portfolio/web/.DS_Store
